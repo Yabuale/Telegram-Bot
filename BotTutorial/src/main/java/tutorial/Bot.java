00b967fdd07c0afe5d43e6e3c145aecdb5103090
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2023-01-21 09:40:26 +0300

    first commit

---
+++ BotTutorial/src/main/java/tutorial/Bot.java
@@ -0,0 +1,41 @@
+package tutorial;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.CopyMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "yabu_yb_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5959902199:AAHIPaHHOvH3sbjUcUzpvFqgTikwWYe5jRg";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+
+        sendText(id, msg.getText()+" contact me @yabu_yb");
+    }
+    //teth
+
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+}
---
+++ BotTutorial/src/main/java/tutorial/Main.java
@@ -0,0 +1,12 @@
+package tutorial;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new Bot());
+    }
+}
