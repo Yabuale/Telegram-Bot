0bce091a318db8cb74cd1c84aa65f52fcc43c6f9
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2023-01-21 10:07:34 +0300

    conditional statment added

--- BotTutorial/src/main/java/tutorial/Bot.java
+++ BotTutorial/src/main/java/tutorial/Bot.java
@@ -6,6 +6,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.Objects;
+
 public class Bot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
@@ -22,8 +24,18 @@ public class Bot extends TelegramLongPollingBot {
         var msg = update.getMessage();
         var user = msg.getFrom();
         var id = user.getId();
+        var name = "BENA";
+        var fullname="BENA ቤናዊ";
 
-        sendText(id, msg.getText()+" contact me @yabu_yb");
+        if(Objects.equals(user.getFirstName().toString(),name.toString())||Objects.equals(user.getFirstName().toString(),fullname.toString())){
+            sendText(id, "Hello MR. "+user.getFirstName()+" Hope you like my bot");
+
+
+        }
+        else {
+
+            sendText(id, "Hello "+user.getFirstName()+" "+msg.getText() + " right back at you \uD83D\uDE09 contact me @yabu_yb");
+        }
     }
     //teth
 
